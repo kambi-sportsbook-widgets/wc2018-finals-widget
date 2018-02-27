1389baf59c61b886bb2a8a6e1af16d56111698e2
Kozanecki Piotr <kopi@jcommerce.pl>
2018-02-27 09:09:55 +0100

    Added default flags and logo locations

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ coreLibrary
       1001159926, // Total goals
       1001642858  // both teams to score
     ], // array with up to 2 numbers, find out the criterionIds of the mock betoffers to use as default
-    // flagBaseUrl: "", // string with the base URL, concatenate with the lowecased englishName of the team + ".svg" to generate final URL (replace spaces with _)
-    // iconUrl: "" // string with the icon URL, should have a default if absent
+    flagBaseUrl: "./assets/flags",   // string with the base URL, concatenate with the lowecased englishName of the team + ".svg" to generate final URL (replace spaces with _)
+    iconUrl: "./assets/world_cup_2018.svg",  // string with the icon URL, should have a default if absent
     backgroundUrl: 'assets/overview-bw-bg-desktop.jpg', // string with the image to use as background-image
     blendWithOperatorColor: true, // determines if background should be blended with operator color. (Normally not wanted if providing own background image)
   })
