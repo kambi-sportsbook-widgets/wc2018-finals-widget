c1b200728b30d4341da9eddaac15706a945d6891
angelicahl88 <angelica@globalmouth.com>
2018-04-03 15:48:16 +0200

    updated date to show today or tomorrow if event is then

--- src/js/Components/Date.jsx
+++ src/js/Components/Date.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { translationModule } from 'kambi-widget-core-library'
+import React from "react";
+import PropTypes from "prop-types";
+import { translationModule } from "kambi-widget-core-library";
 
-const t = translationModule.getTranslation.bind(translationModule)
+const t = translationModule.getTranslation.bind(translationModule);
 
-const DateComponent = ({ date, short = false }) => {
+/**
+ * Pads with leading 0s to ensure number is two digit.
+ * @param {number} n Number to pad
+ */
+const pad = function(n) {
+  return n < 10 ? "0" + n : n;
+};
 
-  const dateObj = new Date(date)
-  const prefix = short? 'Short': ''
+/**
+ * Returns 'today', 'tomorrow' or formatted date if after that
+ * @param {string} startTime
+ */
+const eventTime = (startTime, short) => {
+  const prefix = short ? "Short" : "";
+  const now = new Date(),
+    date = new Date(startTime);
 
-  return (
-    <p>
-      { t('day' + prefix + dateObj.getDay()) + ' ' }
-      { dateObj.getDate() + ' ' }
-      { t('month' + prefix + dateObj.getMonth()) + ' ' }
-      { dateObj.getHours() }:
-      { dateObj.getMinutes() === 0? '00': dateObj.getMinutes() }
-    </p>
-  )
-}
+  const soonStr = (function() {
+    if (now.getDate() === date.getDate()) {
+      return t("today");
+    } else if (now.getDate() === date.getDate() - 1) {
+      return t("tomorrow");
+    } else {
+      return "";
+    }
+  })();
+
+  if (soonStr.length > 0) {
+    return `${soonStr} ${pad(date.getHours())}:${pad(date.getMinutes())}`;
+  }
+
+  return `${t("day" + prefix + date.getDay())} ${date.getDate()} ${t(
+    "month" + prefix + date.getMonth()
+  )} ${pad(date.getHours())}:${pad(date.getMinutes())}`;
+};
+
+const DateComponent = ({ date, short = false }) => (
+  <p>{eventTime(date, short)}</p>
+);
 
 DateComponent.propTypes = {
-  date: PropTypes.oneOfType([
-    PropTypes.number, PropTypes.string
-  ]).isRequired,
+  date: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
   short: PropTypes.bool
-}
+};
 
-export default DateComponent
+export default DateComponent;
