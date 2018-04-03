fed2f5b2d344671a1c17def1b3b7846507d77bba
angelicahl88 <angelica@globalmouth.com>
2018-04-03 14:02:18 +0200

    added widget height adaptiation on resize

--- src/js/Components/EventWidget.jsx
+++ src/js/Components/EventWidget.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { widgetModule, translationModule } from 'kambi-widget-core-library'
-import styles from './EventWidget.scss'
-import Participants from './Participants'
-import BetOffers from './BetOffers'
+import React from "react";
+import PropTypes from "prop-types";
+import { widgetModule, translationModule } from "kambi-widget-core-library";
+import styles from "./EventWidget.scss";
+import Participants from "./Participants";
+import BetOffers from "./BetOffers";
 
 /**
  * Navigate to event
@@ -13,13 +13,13 @@ const navigateToEvent = event => {
   if (event.event.liveBetOffers) {
     // Navigate to live event
     // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-widgetModule.html#.navigateToLiveEvent__anchor
-    widgetModule.navigateToLiveEvent(event.event.id)
+    widgetModule.navigateToLiveEvent(event.event.id);
   } else {
     // Navigate to prelive event
     // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-widgetModule.html#.navigateToEvent__anchor
-    widgetModule.navigateToEvent(event.event.id)
+    widgetModule.navigateToEvent(event.event.id);
   }
-}
+};
 
 /**
  * Check if the event is live and return a label to be added to the title
@@ -30,33 +30,39 @@ const liveLabel = event => {
   if (event.event.liveBetOffers) {
     // Translate a string
     // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-translationModule.html#.getTranslation__anchor
-    return ' - ' + translationModule.getTranslation('liveRightNow')
+    return " - " + translationModule.getTranslation("liveRightNow");
   }
-  return ''
-}
+  return "";
+};
 
 class EventWidget extends React.Component {
   /**
    * Called after the component is mounted
    **/
   componentDidMount() {
-    // Adjust widget height
-    // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-widgetModule.html#.adaptWidgetHeight__anchor
-    widgetModule.adaptWidgetHeight()
+    this.adaptWidgetHeight();
+    window.addEventListener("resize", this.adaptWidgetHeight);
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.adaptWidgetHeight);
+  }
+
+  adaptWidgetHeight = () => {
+    // Adjust widget height
+    // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-widgetModule.html#.adaptWidgetHeight__anchor
+    widgetModule.adaptWidgetHeight();
+  };
+
   render() {
-    let sliceIndex = this.props.event.betOffers.length > 2? 2: 1
-    const betOffersLeft = this.props.event.betOffers.slice(0, sliceIndex)
-    const betOffersRight = this.props.event.betOffers.slice(sliceIndex)
+    let sliceIndex = this.props.event.betOffers.length > 2 ? 2 : 1;
+    const betOffersLeft = this.props.event.betOffers.slice(0, sliceIndex);
+    const betOffersRight = this.props.event.betOffers.slice(sliceIndex);
 
     return (
-      <div >
+      <div>
         <div className={styles.container}>
-          <BetOffers
-            betOffers={betOffersLeft}
-            event={this.props.event.event}
-          />
+          <BetOffers betOffers={betOffersLeft} event={this.props.event.event} />
           <Participants
             event={this.props.event.event}
             flagBaseUrl={this.props.flagBaseUrl}
@@ -69,14 +75,14 @@ class EventWidget extends React.Component {
           />
         </div>
       </div>
-    )
+    );
   }
 }
 
 EventWidget.propTypes = {
   event: PropTypes.object.isRequired,
   flagBaseUrl: PropTypes.string,
-  iconUrl: PropTypes.string,
-}
+  iconUrl: PropTypes.string
+};
 
-export default EventWidget
+export default EventWidget;
