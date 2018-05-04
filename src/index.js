d329c1c999a51ccb6b137f0781b6fc28b76dfdb6
angelicahl88 <angelica@globalmouth.com>
2018-05-04 10:44:45 +0200

    fixed bug with date logic preventing final from showing

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { coreLibrary, widgetModule } from "kambi-widget-core-library";
-import MainComponent from "./js/Components/MainComponent";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { coreLibrary, widgetModule } from 'kambi-widget-core-library'
+import MainComponent from './js/Components/MainComponent'
 
-import getWCEventData from "./js/Services/Kambi";
+import getWCEventData from './js/Services/Kambi'
 
-import "./scss/app.scss";
-import "./index.html";
+import './scss/app.scss'
+import './index.html'
 
 // Initialize kambi's api
 // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-coreLibrary.html#.init__anchor
 coreLibrary
   .init({
-    widgetTrackingName: "champion-league-finals",
+    widgetTrackingName: 'champion-league-finals',
     additionalBetOffersCriterionIds: [
       1001159926, // Total goals
-      1001642858 // both teams to score
+      1001642858, // both teams to score
     ], // array with up to 2 numbers, find out the criterionIds of the mock betoffers to use as default
-    flagBaseUrl: "", // string with the base URL, concatenate with the lowecased englishName of the team + ".svg" to generate final URL (replace spaces with _)
+    flagBaseUrl: '', // string with the base URL, concatenate with the lowecased englishName of the team + ".svg" to generate final URL (replace spaces with _)
     iconUrl:
-      "https://d1fqgomuxh4f5p.cloudfront.net/tournamentdata/worldcup2018/icons/champions_league.svg", // string with the icon URL, should have a default if absent
+      'https://d1fqgomuxh4f5p.cloudfront.net/tournamentdata/worldcup2018/icons/champions_league.svg', // string with the icon URL, should have a default if absent
     backgroundUrl:
-      "https://d1fqgomuxh4f5p.cloudfront.net/tournamentdata/worldcup2018/overview-bw-bg-desktop.jpg", // string with the image to use as background-image
+      'https://d1fqgomuxh4f5p.cloudfront.net/tournamentdata/worldcup2018/overview-bw-bg-desktop.jpg', // string with the image to use as background-image
     blendWithOperatorColor: true, // determines if background should be blended with operator color. (Normally not wanted if providing own background image)
     fetchData: {
-      baseFilter: "football/champions_league",
+      baseFilter: 'football/champions_league',
       qualifyCriterionId: [1001159599], // Team to go through from the match
-      finalCriterionId: [1001221607] // match for third place not occuring in this tournament
+      finalCriterionId: [1001221607], // match for third place not occuring in this tournament
     },
     fetchDates: {
       quarterFinals: {
-        start: "2018-04-03T00:00",
-        end: "2018-04-12T23:59"
+        start: '2018-04-03T00:00',
+        end: '2018-04-12T23:59',
       },
       semiFinals: {
-        start: "2018-04-24T00:00",
-        end: "2018-05-02T23:59"
+        start: '2018-04-24T00:00',
+        end: '2018-05-02T23:59',
       },
       finals: {
-        start: "2018-05-26T00:00"
-      }
-    }
+        start: '2018-05-26T00:00',
+      },
+    },
   })
   .then(() => {
     return getWCEventData(
       coreLibrary.args.additionalBetOffersCriterionIds,
       coreLibrary.args.fetchData,
       coreLibrary.args.fetchDates
-    );
+    )
   })
   .then(data => {
     // Renders the widget
@@ -57,13 +57,13 @@ coreLibrary
         backgroundUrl: coreLibrary.args.backgroundUrl,
         blendWithOperatorColor: coreLibrary.args.blendWithOperatorColor,
         flagBaseUrl: coreLibrary.args.flagBaseUrl,
-        iconUrl: coreLibrary.args.iconUrl
+        iconUrl: coreLibrary.args.iconUrl,
       }),
-      document.getElementById("root")
-    );
+      document.getElementById('root')
+    )
   })
   .catch(error => {
-    widgetModule.removeWidget();
-    console.debug("event widget:", error);
-    throw error;
-  });
+    widgetModule.removeWidget()
+    console.debug('event widget:', error)
+    throw error
+  })
--- src/js/Services/Kambi.js
+++ src/js/Services/Kambi.js
@@ -1,7 +1,7 @@
 import {
-  offeringModule
+  offeringModule,
   // translationModule
-} from "kambi-widget-core-library";
+} from 'kambi-widget-core-library'
 
 // const t = translationModule.getTranslation.bind(translationModule)
 
@@ -10,32 +10,32 @@ import {
  * @returns {Promise} promise of a Kambi event object containing only required betoffers
 */
 const getWCEventData = (additionalBetOffersCriterionIds, data, dates) => {
-  let finalEvent = [];
-  let eventType;
-  let competitionEventsNum;
+  let finalEvent = []
+  let eventType
+  let competitionEventsNum
 
   // get all events from the world cup
   return Promise.all([
     offeringModule.getEventsByFilter(data.baseFilter),
-    offeringModule.getEventsByFilter(data.baseFilter + "/all/all/competitions")
+    offeringModule.getEventsByFilter(data.baseFilter + '/all/all/competitions'),
   ])
     .then(responses => {
-      const events = responses[0];
-      const tournamentEvents = responses[1];
+      const events = responses[0]
+      const tournamentEvents = responses[1]
       // Figure out if we have an array of events or a single one
       if (events.events) {
         // get not-started matches only
         const games = events.events.filter(event => {
           return (
-            event.event.type === "ET_MATCH" &&
-            event.event.state === "NOT_STARTED"
-          );
-        });
+            event.event.type === 'ET_MATCH' &&
+            event.event.state === 'NOT_STARTED'
+          )
+        })
 
-        const now = new Date();
-        let quarterFinals = [];
-        let semiFinals = [];
-        let finals = [];
+        const now = new Date()
+        let quarterFinals = []
+        let semiFinals = []
+        let finals = []
 
         // choose games based on dates in Moscow time zone
         games.forEach(game => {
@@ -45,192 +45,190 @@ const getWCEventData = (additionalBetOffersCriterionIds, data, dates) => {
             new Date(dates.quarterFinals.start) < new Date(game.event.start) &&
             new Date(game.event.start) < new Date(dates.quarterFinals.end)
           ) {
-            quarterFinals.push(game);
-            eventType = "quaterFinals";
+            quarterFinals.push(game)
+            eventType = 'quaterFinals'
           } else if (
             // Finals dates span with padding
             now < new Date(dates.semiFinals.end) &&
             new Date(dates.semiFinals.start) < new Date(game.event.start) &&
             new Date(game.event.start) < new Date(dates.semiFinals.end)
           ) {
-            semiFinals.push(game);
-            eventType = "semiFinals";
+            semiFinals.push(game)
+            eventType = 'semiFinals'
           } else if (
             // Finals dates span with padding
-            now < new Date(dates.finals.end) &&
             new Date(dates.finals.start) < new Date(game.event.start)
           ) {
-            finals.push(game);
-            eventType = "finals";
+            finals.push(game)
+            eventType = 'finals'
           }
-        });
+        })
 
-        let gamesToLookup;
+        let gamesToLookup
         if (quarterFinals.length > 0) {
-          gamesToLookup = quarterFinals;
+          gamesToLookup = quarterFinals
         } else if (semiFinals.length > 0) {
-          gamesToLookup = semiFinals;
+          gamesToLookup = semiFinals
         } else if (finals.length > 0) {
-          gamesToLookup = finals.reverse();
+          gamesToLookup = finals.reverse()
         } else {
           throw new Error(
             `No events available for supplied filter: ${data.baseFilter}`,
             events.event
-          );
+          )
         }
         // Prepare games promises
         const promisesMatches = gamesToLookup.map(game => {
-          return offeringModule.getEvent(game.event.id);
-        });
+          return offeringModule.getEvent(game.event.id)
+        })
 
         // Add tournament events promises
         // Filter events based on citerion ids that are selected depending on the stage of the tournament
         const criteria =
-          eventType !== "finals"
+          eventType !== 'finals'
             ? data.qualifyCriterionId
-            : data.finalCriterionId;
+            : data.finalCriterionId
         const competitionEvents = tournamentEvents.events.filter(event => {
           return event.betOffers.length > 0
             ? criteria.indexOf(event.betOffers[0].criterion.id) >= 0
-            : false;
-        });
+            : false
+        })
 
-        competitionEventsNum = competitionEvents.length;
+        competitionEventsNum = competitionEvents.length
         const promisesCompetition = competitionEvents.map(event => {
-          return offeringModule.getEvent(event.event.id);
-        });
+          return offeringModule.getEvent(event.event.id)
+        })
 
         // Get all betoffers of all the games
         // http://kambi-sportsbook-widgets.github.io/widget-core-library/module-offeringModule.html#.getLiveEventsByFilter__anchor
-        return Promise.all([...promisesMatches, ...promisesCompetition]);
+        return Promise.all([...promisesMatches, ...promisesCompetition])
       }
 
       throw new Error(
         `No events available for supplied filter: ${data.baseFilter}`,
         events.event
-      );
+      )
     })
     .then(responses => {
       const events = competitionEventsNum
         ? responses.slice(0, competitionEventsNum * -1)
-        : responses.slice(0);
+        : responses.slice(0)
       const competitionEventData = competitionEventsNum
         ? responses.slice(competitionEventsNum * -1)
-        : [];
+        : []
 
       events.forEach((event, index) => {
         // Try to find a bet offer matching the qualify criterion id for each event
         event.toQualifyBetOffer = event.betOffers.find(
           offer => data.qualifyCriterionId.indexOf(offer.criterion.id) >= 0
-        );
+        )
 
         // Filter bet offers based on criterion ids provided in params
-        let newBetOffers = [];
+        let newBetOffers = []
         // Add main offer
-        const mainBetoffer = event.betOffers.find(offer => offer.main);
+        const mainBetoffer = event.betOffers.find(offer => offer.main)
         if (mainBetoffer) {
-          newBetOffers.push(mainBetoffer);
+          newBetOffers.push(mainBetoffer)
         }
 
         // Add selected bet offers
         additionalBetOffersCriterionIds.forEach(crit => {
           let result = event.betOffers.find(
             offer => offer.criterion.id === crit
-          );
-          if (result) newBetOffers.push(result);
-        });
+          )
+          if (result) newBetOffers.push(result)
+        })
 
-        event.betOffers = newBetOffers;
-        finalEvent[index] = event;
-      });
+        event.betOffers = newBetOffers
+        finalEvent[index] = event
+      })
 
       // Inject competition events
       const result = finalEvent.map((event, index) => {
         // Extract betting offer for Playing countries
         const countries = event.event.englishName
-          .split("-")
-          .map(item => item.trim());
-        event.event.countriesEnglishNames = countries;
+          .split('-')
+          .map(item => item.trim())
+        event.event.countriesEnglishNames = countries
 
         // Get bet offers and inject them into the displayed event
         // Offers are being overwritten so only the last event from the criterion id list is actually displayed
-        let tournamentBetOffer;
+        let tournamentBetOffer
         competitionEventData.forEach(competitionEvent => {
-          let betOffer;
-          if (eventType !== "finals") {
+          let betOffer
+          if (eventType !== 'finals') {
             betOffer = competitionEvent.betOffers.find(offer => {
-              return offer.to === 2;
-            });
+              return offer.to === 2
+            })
             if (!betOffer) {
-              betOffer = competitionEvent.betOffers[0];
+              betOffer = competitionEvent.betOffers[0]
             }
           } else {
-            betOffer = competitionEvent.betOffers[0];
+            betOffer = competitionEvent.betOffers[0]
           }
 
-          const tournamentBetOfferCandidate = Object.assign({}, betOffer);
+          const tournamentBetOfferCandidate = Object.assign({}, betOffer)
           tournamentBetOfferCandidate.outcomes = tournamentBetOfferCandidate.outcomes.filter(
             offer => {
-              return countries.indexOf(offer.englishLabel) >= 0;
+              return countries.indexOf(offer.englishLabel) >= 0
             }
-          );
+          )
           if (tournamentBetOfferCandidate.outcomes.length > 0) {
-            tournamentBetOffer = tournamentBetOfferCandidate;
+            tournamentBetOffer = tournamentBetOfferCandidate
           }
-        });
+        })
 
         // If no bet offers found in tournament event try to get a qualification bet offer from the event
         if (!tournamentBetOffer) {
           if (event.toQualifyBetOffer) {
-            tournamentBetOffer = event.toQualifyBetOffer;
+            tournamentBetOffer = event.toQualifyBetOffer
           } else {
             // Find the match beetween the same teams
             const rematch = events.find(
               ev =>
                 event.event.homeName === ev.event.awayName &&
                 event.event.awayName === ev.event.homeName
-            );
-            tournamentBetOffer = { ...rematch.toQualifyBetOffer };
+            )
+            tournamentBetOffer = { ...rematch.toQualifyBetOffer }
             // Swap outcomes order and labels
             tournamentBetOffer.outcomes = tournamentBetOffer.outcomes
               .map(outcome => {
-                return { ...outcome };
+                return { ...outcome }
               })
-              .reverse();
+              .reverse()
 
             for (let i = 0; i < tournamentBetOffer.outcomes.length / 2; i++) {
-              const iRev = tournamentBetOffer.outcomes.length - i - 1;
-              const tempLabel = tournamentBetOffer.outcomes[iRev].label;
+              const iRev = tournamentBetOffer.outcomes.length - i - 1
+              const tempLabel = tournamentBetOffer.outcomes[iRev].label
               const tempEnglishLabel =
-                tournamentBetOffer.outcomes[iRev].englishLabel;
-              const tempType = tournamentBetOffer.outcomes[iRev].type;
+                tournamentBetOffer.outcomes[iRev].englishLabel
+              const tempType = tournamentBetOffer.outcomes[iRev].type
 
               tournamentBetOffer.outcomes[iRev].label =
-                tournamentBetOffer.outcomes[i].label;
+                tournamentBetOffer.outcomes[i].label
               tournamentBetOffer.outcomes[iRev].englishLabel =
-                tournamentBetOffer.outcomes[i].englishLabel;
+                tournamentBetOffer.outcomes[i].englishLabel
               tournamentBetOffer.outcomes[iRev].type =
-                tournamentBetOffer.outcomes[i].type;
+                tournamentBetOffer.outcomes[i].type
 
-              tournamentBetOffer.outcomes[i].label = tempLabel;
-              tournamentBetOffer.outcomes[i].englishLabel = tempEnglishLabel;
-              tournamentBetOffer.outcomes[i].type = tempType;
+              tournamentBetOffer.outcomes[i].label = tempLabel
+              tournamentBetOffer.outcomes[i].englishLabel = tempEnglishLabel
+              tournamentBetOffer.outcomes[i].type = tempType
             }
           }
         }
 
         // Add new bet offers to event object to pass to React
-        if (tournamentBetOffer)
-          event.betOffers.splice(1, 0, tournamentBetOffer);
+        if (tournamentBetOffer) event.betOffers.splice(1, 0, tournamentBetOffer)
 
-        return event;
-      });
+        return event
+      })
 
       // Sort by starting date
-      result.sort((a, b) => new Date(a.event.start) - new Date(b.event.start));
+      result.sort((a, b) => new Date(a.event.start) - new Date(b.event.start))
 
-      return result;
-    });
-};
+      return result
+    })
+}
 
-export default getWCEventData;
+export default getWCEventData
