3da9d843486ad9732a11254b00075cbe2e7a39f1
Owain <owain@Owains-MacBook-Pro.local>
2018-05-14 16:53:18 +0200

    fixes WC2W-86 and updated dependency versions

--- src/js/Components/Date.jsx
+++ src/js/Components/Date.jsx
@@ -5,26 +5,23 @@ import { translationModule } from 'kambi-widget-core-library'
 const t = translationModule.getTranslation.bind(translationModule)
 
 const DateComponent = ({ date, short = false }) => {
-
   const dateObj = new Date(date)
-  const prefix = short? 'Short': ''
+  const prefix = short ? 'Short' : ''
 
   return (
     <p>
-      { t('day' + prefix + dateObj.getDay()) + ' ' }
-      { dateObj.getDate() + ' ' }
-      { t('month' + prefix + dateObj.getMonth()) + ' ' }
-      { dateObj.getHours() }:
-      { dateObj.getMinutes() === 0? '00': dateObj.getMinutes() }
+      {`${t('day' + dateObj.getDay())} ${dateObj.getDate()} ${t(
+        'month' + dateObj.getMonth()
+      )} ${dateObj.getHours()}.${
+        dateObj.getMinutes() === 0 ? '00' : dateObj.getMinutes()
+      }`}
     </p>
   )
 }
 
 DateComponent.propTypes = {
-  date: PropTypes.oneOfType([
-    PropTypes.number, PropTypes.string
-  ]).isRequired,
-  short: PropTypes.bool
+  date: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  short: PropTypes.bool,
 }
 
 export default DateComponent
