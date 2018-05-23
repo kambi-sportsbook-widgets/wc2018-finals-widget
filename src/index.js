da6c64003a97b84e07c0c6bc76794ac9c5b8122a
angelicahl88 <angelica@globalmouth.com>
2018-05-23 15:45:07 +0200

    began changing data fetch logic

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ import './index.html'
 coreLibrary
   .init({
     widgetTrackingName: 'wc2018-finals',
-    additionalBetOffersCriterionIds: [
-      1001159926, // Total goals
-      1001642858, // both teams to score
-    ], // array with up to 2 numbers, find out the criterionIds of the mock betoffers to use as default
+    // additionalBetOffersCriterionIds: [
+    //   1001159926, // Total goals
+    //   1001642858, // both teams to score
+    // ], // array with up to 2 numbers, find out the criterionIds of the mock betoffers to use as default
     flagBaseUrl:
       'https://d1fqgomuxh4f5p.cloudfront.net/tournamentdata/worldcup2018/icons', // string with the base URL, concatenate with the lowecased englishName of the team + ".svg" to generate final URL (replace spaces with _)
     iconUrl:
@@ -24,14 +24,47 @@ coreLibrary
     backgroundUrl:
       'https://d1fqgomuxh4f5p.cloudfront.net/tournamentdata/worldcup2018/overview-bw-bg-desktop.jpg', // string with the image to use as background-image
     blendWithOperatorColor: true, // determines if background should be blended with operator color. (Normally not wanted if providing own background image)
-    fetchData: {
-      baseFilter: '/football/world_cup_2018',
-      qualifyCriterionId: [1004240929],
-      finalCriterionId: [1002978411, 1004240929],
+    // fetchData: {
+    //   baseFilter: '/football/world_cup_2018',
+    //   qualifyCriterionId: [1004240929],
+    //   finalCriterionId: [1002978411, 1004240929],
+    // },
+
+    filter: '/football/world_cup_2018',
+    criterionIds: {
+      topLeftBetOffer: {
+        // Full time
+        quarter: [1001159858],
+        semi: [1001159858],
+        final: [1001159858],
+        fallBack: [1001159858],
+      },
+      topRightBetOffer: {
+        // Total goals
+        quarter: [1001159926],
+        semi: [1001159926],
+        final: [1001159926],
+        fallBack: [1001159926],
+      },
+      bottomLeftBetOffer: {
+        // Qualify / Win Bronze/ Win the Trophy
+        quarter: [1001159599],
+        semi: [1002978411],
+        final: [1001159600],
+        fallBack: [1001159599],
+      },
+      bottomRightBetOffer: {
+        // Both teams to score
+        quarter: [1001642858],
+        semi: [1001642858],
+        final: [1001642858],
+        fallBack: [1001642858],
+      },
     },
+
     // Data for World Cup 2018
     // // Date span for World Cup 2018
-    // fetchDates: {
+    // tournamentDates: {
     //   quarterFinals: {
     //     start: '2018-07-06T00:00',
     //     end: '2018-07-07T23:59'
@@ -47,7 +80,7 @@ coreLibrary
     // // Date span for World Cup 2018
 
     // Date span for test purposes to be used with World Cup data
-    fetchDates: {
+    tournamentDates: {
       quarterFinals: {
         start: '2018-06-16T00:00',
         end: '2018-06-16T23:59',
@@ -63,11 +96,13 @@ coreLibrary
     // Date span for test purposes to be used with World Cup data
   })
   .then(() => {
-    return getWCEventData(
-      coreLibrary.args.additionalBetOffersCriterionIds,
-      coreLibrary.args.fetchData,
-      coreLibrary.args.fetchDates
-    )
+    const { tournamentDates, filter, criterionIds } = coreLibrary.args
+    return getTournamentData(filter, criterionIds, tournamentDates)
+    // return getWCEventData(
+    //   coreLibrary.args.additionalBetOffersCriterionIds,
+    //   coreLibrary.args.fetchData,
+    //   coreLibrary.args.fetchDates
+    // )
   })
   .then(data => {
     // Renders the widget
--- src/js/Services/Kambi.js
+++ src/js/Services/Kambi.js
@@ -1,6 +1,54 @@
 import { offeringModule } from 'kambi-widget-core-library'
 
-const getEventData = () => {}
+const getEventData = (filter, criterionIds, dateRanges) => {
+  // fetch tournament events and outrights
+  const dataRequests = [
+    offeringModule.getEventsByFilter(filter), // tournament matches
+    offeringModule.getEventsByFilter(`${filter}/all/all/competitions`), // tournament competitions (e.g. golden boot, tournament winner etc.)
+  ]
+
+  return Promise.all(dataRequests)
+    .then(tournamentData => {
+      if (tournamentData == null) {
+        throw new Error(
+          `No tournament data available for supplied filters: ${filter}, ${filter}/all/all/competitions`
+        )
+      }
+      const tournamentEvents = tournamentData[0].events
+      const tournamentCompetitions = tournamentData[1].events
+
+      // filter events by date range
+
+      // get competitions to render by criterionId (hardcoded)
+      let leftWidget
+      let rightWidget
+      tournamentCompetitions.forEach(competition => {
+        competition.betOffers.forEach(betOffer => {
+          if (competition.event.id === leftWidgetInput.eventId) {
+            if (betOffer.criterion.id === leftWidgetInput.criterionId) {
+              leftWidget = competition
+            }
+            return
+          } else if (competition.event.id === rightWidgetInput.eventId) {
+            if (betOffer.criterion.id === rightWidgetInput.criterionId) {
+              rightWidget = competition
+            }
+            return
+          }
+          return
+        })
+      })
+
+      return Promise.resolve({
+        events: tournamentEvents,
+        competitions: { leftWidget, rightWidget },
+      })
+    })
+    .catch(err => {
+      return Promise.reject(err)
+    })
+}
+
 /*
  * @property additionalBetOffersCriterionIds {Array.<number>} an array containing up to two criterion ids to be displayed in the widget
  * @returns {Promise} promise of a Kambi event object containing only required betoffers
